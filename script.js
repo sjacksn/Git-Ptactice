43a4f23c8d73c940e6c6fd53dff54d2926011cf5
sjacksn <71403273+sjacksn@users.noreply.github.com>
2021-01-26 21:34:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$('#btnRun').click(function() {
+
+	$.ajax({
+		url: "getCountryInfo.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			country: $('#selCountry').val(),
+			lang: $('#selLanguage').val()
+		},
+		success: function(result) {
+
+			console.log(result);
+
+			if (result.status.name == "ok") {
+
+				$('#txtContinent').html(result['data']['continent']);
+				$('#txtCapital').html(result['data']['capital']);
+				$('#txtGeonameId').html(result['data']['geonameId']);
+				$('#txtCountryCode').html(result['data']['countryCode']);
+				$('#txtCurrencyCode').html(result['data']['currencyCode']);
+
+			}
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			// your error code
+		}
+	}); 
+
+
+});
